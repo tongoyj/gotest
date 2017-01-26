5e2ea90b0055f02094968822e9ca1ada15d4bc12
Ubuntu <ubuntu@ip-10-0-1-47.ap-southeast-2.compute.internal>
2017-01-26 15:21:19 +0000

    variables

--- variables.go
+++ variables.go
@@ -3,22 +3,15 @@ package main
 import "fmt"
 func main() {
 
-    var a string = "initial"
-    fmt.Println(a)
+    var a string = "www."
+    var b string = "BibleStory."
+    var c string = "info"
+    var d string = " - Know Jesus Now..."
+   
+    fmt.Println(a+b+c+d)
 
-    var b, c int = 1, 2
-    fmt.Println(b, c)
-
-    var d = true
-    fmt.Println(d)
-
-    var e int
-    fmt.Println(e)
-
-    f := "short"
-    fmt.Println(f)
 }
 
-
+# www.biblestory.info sample variable in GoLang
 
 
